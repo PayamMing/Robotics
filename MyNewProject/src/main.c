1c4271e7b0cdbefacc620f98b8a6f1915193e8ae
Ming Peng <ming.peng@sigmatechnology.se>
2018-11-09 14:27:00 +0100

    Motor move forward and backward, speed control, etc

--- MyNewProject/src/main.c
+++ MyNewProject/src/main.c
@@ -30,6 +30,9 @@ SOFTWARE.
 /* Includes */
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
 
 /* Private macro */
 /* Private variables */
@@ -43,40 +46,197 @@ SOFTWARE.
 **
 **===========================================================================
 */
-int main(void)
-{
-  int i = 0;
+void TM_LEDS_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct;
+    /* Clock for GPIOD */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-  /**
-  *  IMPORTANT NOTE!
-  *  The symbol VECT_TAB_SRAM needs to be defined when building the project
-  *  if code has been located to RAM and interrupts are used. 
-  *  Otherwise the interrupt table located in flash will be used.
-  *  See also the <system_*.c> file and how the SystemInit() function updates 
-  *  SCB->VTOR register.  
-  *  E.g.  SCB->VTOR = 0x20000000;  
-  */
+	/* Alternating functions for pins */
+	//GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+	//GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
 
-  /* TODO - Add your application code here */
+	/*Set pins */
+	GPIO_InitStruct.GPIO_Pin =  GPIO_Pin_13 | GPIO_Pin_14;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-  /* Initialize LEDs */
-  STM_EVAL_LEDInit(LED3);
-  STM_EVAL_LEDInit(LED4);
-  STM_EVAL_LEDInit(LED5);
-  STM_EVAL_LEDInit(LED6);
+}
 
-  /* Turn on LEDs */
-  STM_EVAL_LEDOn(LED3);
-  STM_EVAL_LEDOn(LED4);
-  STM_EVAL_LEDOn(LED5);
-  STM_EVAL_LEDOn(LED6);
+void TM_TIMER_Init(void) {
+	TIM_TimeBaseInitTypeDef TIM_BaseStruct;
+	/* Enable clock for TIM4 */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 
-  /* Infinite loop */
-  while (1)
-  {
+/*    
+    TIM4 is connected to APB1 bus, which has on F407 device 42MHz clock                 
+    But, timer has internal PLL, which double this frequency for timer, up to 84MHz     
+    Remember: Not each timer is connected to APB1, there are also timers connected     
+    on APB2, which works at 84MHz by default, and internal PLL increase                 
+    this to up to 168MHz                                                             
+    
+    Set timer prescaller
+    Timer count frequency is set with
+    
+    timer_tick_frequency = Timer_default_frequency / (prescaller_set + 1)        
+    
+    In our case, we want a max frequency for timer, so we set prescaller to 0         
+    And our timer will have tick frequency        
+    
+    timer_tick_frequency = 84000000 / (0 + 1) = 84000000
+*/
+	TIM_BaseStruct.TIM_Prescaler = 0;
+	/* Count up */
+	TIM_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+/*
+    Set timer period when it have reset
+    First you have to know max value for timer
+    In our case it is 16bit = 65535
+    To get your frequency for PWM, equation is simple
+    
+    PWM_frequency = timer_tick_frequency / (TIM_Period + 1)
+    
+    If you know your PWM frequency you want to have timer period set correct
+    
+    TIM_Period = timer_tick_frequency / PWM_frequency - 1
+    
+    In our case, for 10Khz PWM_frequency, set Period to
+    
+    TIM_Period = 84000000 / 10000 - 1 = 8399
+    
+    If you get TIM_Period larger than max timer value (in our case 65535),
+    you have to choose larger prescaler and slow down timer tick frequency
+*/
+	TIM_BaseStruct.TIM_Period = 8399; /* 10kHz PWM */
+	TIM_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_BaseStruct.TIM_RepetitionCounter = 0;
+	/* Initialize TIM4 */
+	TIM_TimeBaseInit(TIM4, &TIM_BaseStruct);
+	/* Start count on TIM4 */
+	TIM_Cmd(TIM4, ENABLE);
+}
 
-	i++;
-  }
+void TM_PWM_Init(__IO uint32_t duty_cycle_right, __IO uint32_t duty_cycle_left) {
+	TIM_OCInitTypeDef TIM_OCStruct;
+
+	/* Common settings */
+
+	/* PWM mode 2 = Clear on compare match */
+	/* PWM mode 1 = Set on compare match */
+	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM2;
+	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
+
+	/*
+    To get proper duty cycle, you have simple equation
+    
+    pulse_length = ((TIM_Period + 1) * DutyCycle) / 100 - 1
+    
+    where DutyCycle is in percent, between 0 and 100%
+    
+    25% duty cycle:     pulse_length = ((8399 + 1) * 25) / 100 - 1 = 2099
+    50% duty cycle:     pulse_length = ((8399 + 1) * 50) / 100 - 1 = 4199
+    75% duty cycle:     pulse_length = ((8399 + 1) * 75) / 100 - 1 = 6299
+    100% duty cycle:    pulse_length = ((8399 + 1) * 100) / 100 - 1 = 8399
+    
+    Remember: if pulse_length is larger than TIM_Period, you will have output HIGH all the time
+	 */
+
+	/*
+	TIM_OCStruct.TIM_Pulse = 2099; // 25% duty cycle
+	TIM_OC1Init(TIM4, &TIM_OCStruct);
+	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	*/
+
+	// Right motor
+	TIM_OCStruct.TIM_Pulse = duty_cycle_right;
+	TIM_OC2Init(TIM4, &TIM_OCStruct);
+	TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+	// Left motor
+	TIM_OCStruct.TIM_Pulse = duty_cycle_left;
+	TIM_OC3Init(TIM4, &TIM_OCStruct);
+	TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+	/*
+	TIM_OCStruct.TIM_Pulse = 8399; // 100% duty cycle
+	TIM_OC4Init(TIM4, &TIM_OCStruct);
+	TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	*/
+}
+
+void Delay(__IO uint32_t nCount){
+	  while(nCount--){
+
+	  }
+}
+
+int main(void) {
+	/* Initialize system */
+	SystemInit();
+	/* Init leds */
+	TM_LEDS_Init();
+	/* Init timer */
+	TM_TIMER_Init();
+	/* Init PWM */
+	TM_PWM_Init(4199, 4199);
+
+
+	//Pin 0
+	GPIO_InitTypeDef GPIO_InitDef;
+	GPIO_InitDef.GPIO_Pin = GPIO_Pin_0;
+	//Mode output
+	GPIO_InitDef.GPIO_Mode = GPIO_Mode_IN;
+	//Output type push-pull
+	GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
+	//With pull down resistor
+	GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_DOWN;
+	//50MHz pin speed
+	GPIO_InitDef.GPIO_Speed = GPIO_Speed_50MHz;
+	//Initialize pin on GPIOA port
+	GPIO_Init(GPIOA, &GPIO_InitDef);
+
+	GPIO_InitDef.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_12 | GPIO_Pin_15;
+	GPIO_InitDef.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitDef.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitDef);
+
+
+
+	GPIO_WriteBit(GPIOD, GPIO_Pin_13, Bit_SET);
+	GPIO_WriteBit(GPIOD, GPIO_Pin_14, Bit_SET);
+
+	//uint8_t button_state;
+
+	while (1) {
+
+		//button_state = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
+		// Move forward STM_EVAL_PBGetState(BUTTON_USER)
+		//if (button_state) {
+			//Motor 1 forward
+			GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_SET);
+			GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_RESET);
+			//Motor 2 forward
+			GPIO_WriteBit(GPIOD, GPIO_Pin_10, Bit_SET);
+			GPIO_WriteBit(GPIOD, GPIO_Pin_8, Bit_RESET);
+			Delay(16800000);
+
+		//} else {
+			GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_RESET);
+			GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_RESET);
+			GPIO_WriteBit(GPIOD, GPIO_Pin_8, Bit_RESET);
+			GPIO_WriteBit(GPIOD, GPIO_Pin_10, Bit_RESET);
+		//}
+			Delay(16800000);
+
+	}
+
 }
 
 
